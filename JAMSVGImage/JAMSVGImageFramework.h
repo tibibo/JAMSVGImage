cef888691218f6dde3d3791b5b0325fef2fca65e
Zoltan Bognar <z.bognar@aliceintokyo.net>
2016-05-25 16:11:18 +0200

    framework support added

---
+++ JAMSVGImage/JAMSVGImageFramework.h
@@ -0,0 +1,23 @@
+//
+//  JAMSVGImage.h
+//  JAMSVGImage
+//
+//  Created by Zoltan Bognar on 25.05.16.
+//  Copyright © 2016 Thirtyseventy Digital s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JAMSVGImage.
+FOUNDATION_EXPORT double JAMSVGImageVersionNumber;
+
+//! Project version string for JAMSVGImage.
+FOUNDATION_EXPORT const unsigned char JAMSVGImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JAMSVGImage/PublicHeader.h>
+
+
+#import <JAMSVGImageFramework/JAMSVGImage.h>
+#import <JAMSVGImageFramework/UIImage+SVG.h>
+#import <JAMSVGImageFramework/JAMSVGButton.h>
+#import <JAMSVGImageFramework/JAMSVGImageView.h>
